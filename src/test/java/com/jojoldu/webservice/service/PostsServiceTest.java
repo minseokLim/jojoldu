abaf4dd9777857c4e210b643a68192cfbc427cf6
mslim8803 <mslim8803@kaoni.com>
2019-06-12 10:35:28 +0900

    아 쫌

--- src/test/java/com/jojoldu/webservice/service/PostsServiceTest.java
+++ src/test/java/com/jojoldu/webservice/service/PostsServiceTest.java
@@ -21,19 +21,19 @@ public class PostsServiceTest {
     @Autowired
     private PostsRepository postsRepository;
 
-//    @Test
-//    public void savePosts() {
-//        PostsSaveRequestDto dto = PostsSaveRequestDto.builder()
-//                .author("mslim")
-//                .content("content")
-//                .title("title")
-//                .build();
-//
-//        postsService.save(dto);
-//
+    @Test
+    public void savePosts() {
+        PostsSaveRequestDto dto = PostsSaveRequestDto.builder()
+                .author("mslim")
+                .content("content")
+                .title("title")
+                .build();
+
+        postsService.save(dto);
+
 //        Posts posts = postsRepository.findAll().get(0);
 //        Assert.assertEquals(dto.getAuthor(), posts.getAuthor());
 //        Assert.assertEquals(dto.getContent(), posts.getContent());
 //        Assert.assertEquals(dto.getTitle(), posts.getTitle());
-//    }
+    }
 }
